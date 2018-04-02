289a1fb065b19ee475a2a2ff61b338568f631faa
Jesse Pinnell (Desktop) <jesse.pinnell@efi.com>
2018-04-01 21:54:44 -0400

    Add grid control to sample app

--- python/driver_sample_app.py
+++ python/driver_sample_app.py
@@ -31,6 +31,8 @@
 # pylint: disable=bad-option-value
 # pylint: disable=import-error
 # pylint: disable=too-many-public-methods
+# pylint: disable=too-many-ancestors
+# pylint: disable=no-self-use
 
 import sys
 sys.path.append("../lib")
@@ -39,6 +41,7 @@ sys.path.append("../python")
 sys.path.append("python")
 
 import wx
+import wx.grid
 import py_driver_sample
 
 ID_ABOUT = 2323
@@ -46,6 +49,64 @@ ID_QUIT = 2324
 ID_RESET = 2325
 ID_READ = 2326
 ID_WRITE = 2327
+
+NUM_ROWS = 16
+NUM_COLUMNS = 8
+
+def get_cell_by_address(address):
+    """ get the cell address from a memory address """
+    return (address // NUM_COLUMNS, address % NUM_COLUMNS)
+
+def get_address_by_cell(row, column):
+    """ get the memory address from a cell address """
+    return (row * NUM_COLUMNS) + column
+
+class Simulator(object):
+    """ loopback simulator for when driver fails to talk to hardware """
+    def __init__(self):
+        self.data = [0] * (NUM_ROWS * NUM_COLUMNS)
+
+    def write(self, address, value):
+        """ shove a value into the fake memory """
+        print "setting 0x{0:x} to 0x{1:x}".format(address, value)
+        self.data[address] = value
+
+    def read(self, address):
+        """ increment a value in the fake memory and return it """
+        self.data[address] = min(self.data[address] + 1, 0xff)
+        return self.data[address]
+
+    def reset(self):
+        """ pretend to reset """
+        print "reset"
+
+class RegisterGrid(wx.grid.Grid):
+    """ Grid matching Table 4.1 in http://ww1.microchip.com/downloads/en/DeviceDoc/20005282B.pdf """
+    def __init__(self, parent):
+        super(RegisterGrid, self).__init__(parent, size=(300, 400))
+        self.CreateGrid(NUM_ROWS, NUM_COLUMNS)
+
+        grid_font = wx.Font(9, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Terminus")
+        self.SetDefaultCellFont(grid_font)
+        self.SetDefaultRowSize(19)
+        self.SetDefaultColSize(30)
+        self.SetFont(grid_font)
+        self.SetRowLabelSize(30)
+
+        self.skip_cell = None
+
+        for row in xrange(NUM_ROWS):
+            self.SetRowLabelValue(row, "{0:04b}".format(row))
+
+        for col in xrange(NUM_COLUMNS):
+            self.SetColLabelValue(col, "{0:04b}".format(col))
+
+
+    def set_value(self, address, value):
+        """ update a value in the grid """
+        cell = get_cell_by_address(address)
+        self.SetCellValue(cell[0], cell[1], "{0:02x}".format(value))
+
 
 class DriverSampleApp(wx.Frame):
     """
@@ -56,22 +117,17 @@ class DriverSampleApp(wx.Frame):
         """
         Create the frame, lay out controls, and set up event handlers
         """
-        wx.Frame.__init__(self, parent, wx.ID_ANY, title, size=(200, 220), style=(wx.CLOSE_BOX))
+        wx.Frame.__init__(self, parent, wx.ID_ANY, title, size=(400, 480), style=(wx.CLOSE_BOX))
 
         main_sizer = wx.BoxSizer(wx.VERTICAL)
 
-        reset_button = wx.Button(self, ID_RESET, "Reset", size=wx.Size(170, 42))
+        reset_button = wx.Button(self, ID_RESET, "Reset", size=wx.Size(120, 20))
         main_sizer.Add(reset_button, 0, wx.CENTER, 0)
 
-        read_sizer = wx.BoxSizer(wx.VERTICAL)
-        read_button = wx.Button(self, ID_READ, "Read", size=wx.Size(170, 42))
-        read_sizer.Add(read_button, 0, wx.CENTER, 0)
-        main_sizer.Add(read_sizer, 0, wx.CENTER, 0)
-
-        write_sizer = wx.BoxSizer(wx.VERTICAL)
-        write_button = wx.Button(self, ID_WRITE, "Write", size=wx.Size(170, 42))
-        write_sizer.Add(write_button, 0, wx.CENTER, 0)
-        main_sizer.Add(write_sizer, 0, wx.CENTER, 0)
+        register_sizer = wx.BoxSizer(wx.VERTICAL)
+        self.register_grid = RegisterGrid(self)
+        register_sizer.Add(self.register_grid, 0, wx.CENTER, 0)
+        main_sizer.Add(register_sizer, 0, wx.CENTER, 0)
 
 
         self.SetSizer(main_sizer)
@@ -90,18 +146,62 @@ class DriverSampleApp(wx.Frame):
         wx.EVT_BUTTON(self, ID_RESET, self.on_reset)
         wx.EVT_MENU(self, ID_ABOUT, self.on_about)
         wx.EVT_MENU(self, ID_QUIT, self.on_quit)
+        self.register_grid.Bind(wx.grid.EVT_GRID_CELL_CHANGE, self.on_grid_changed)
+        self.register_grid.Bind(wx.grid.EVT_GRID_SELECT_CELL, self.on_grid_cell_selected)
+        self.register_grid.Bind(wx.grid.EVT_GRID_LABEL_LEFT_CLICK, self.on_grid_edited)
 
-        self.Show(True)
+        self.skip_cell = None
+
+        self.timer = wx.Timer(self)
+        self.Bind(wx.EVT_TIMER, self.on_poll_timer)
 
         try:
             self.log = py_driver_sample.Log()
             self.driver = py_driver_sample.Driver()
+            self.timer.Start(5000)
         except Exception as ex:
-            dialog = wx.MessageDialog(self, "Failed to initialize:\n" + str(ex), "Error", wx.ICON_EXCLAMATION)
+            dialog = wx.MessageDialog(self, "Failed to initialize:\n" + str(ex)\
+                + "\n\n*** Using simulator ***", "Error", wx.ICON_EXCLAMATION)
             dialog.ShowModal()
             dialog.Destroy()
-            self.Close()
+            self.driver = Simulator()
+            self.timer.Start(2500)
 
+        self.on_poll_timer(None)
+        self.Show(True)
+
+    def on_grid_changed(self, event):
+        """ handle a value in the grid being changed """
+        try:
+            value = int(self.register_grid.GetCellValue(event.Row, event.Col).encode("ascii", "ignore"), 16)
+            if value > 0xff:
+                raise Exception("Value must be in [0x0..0xff]")
+            address = get_address_by_cell(event.Row, event.Col)
+
+            self.driver.write(address, value)
+        except Exception as ex:
+            dialog = wx.MessageDialog(self, "Invalid entry: " + str(ex), "Error", wx.ICON_EXCLAMATION)
+            dialog.ShowModal()
+            dialog.Destroy()
+        self.skip_cell = None
+        event.Skip()
+
+    def on_grid_cell_selected(self, event):
+        """ a cell has been selected so skip updating it """
+        self.skip_cell = (event.Row, event.Col)
+
+    def on_grid_edited(self, event):
+        """ ignore handling changes until enter is pressed """
+        event.Skip()
+
+    def on_poll_timer(self, event):
+        """ read all the register values and update them in the grid """
+        for i in range(0, NUM_ROWS * NUM_COLUMNS):
+            cell = get_cell_by_address(i)
+            if cell != self.skip_cell:
+                self.register_grid.set_value(i, self.driver.read(i))
+            else:
+                print "editing " + str(cell) + ", skipping update"
 
     def on_reset(self, event):
         """
